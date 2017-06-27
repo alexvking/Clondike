622f4b45199645685d27bce9b0ac76e7208ba3db
Alex King <alexvking@icloud.com>
2017-06-27 11:11:35 -0700

    Initial Commit

---
+++ Clondike/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clondike
+//
+//  Created by Alex King on 6/27/17.
+//  Copyright © 2017 Alex King. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
